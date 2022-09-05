084d4d5b0f9acbbff151a3f41fbd548c153b583c
wspawned <wspawned@gmail.com>
2022-09-05 05:38:02 +0300

    input form

---
+++ README.md
@@ -0,0 +1,48 @@
+# Kullanıcı Girdileri
+
+* Kredi tutarı (Ana para) 
+* Taksit sayısı
+* Kâr oranı
+* Taksit aralığı seçimi → aylık, haftalık, yıllık
+* Vergi oranı → bsmv ve kkdf
+
+# Amaçlar
+
+Bir formda kullanıcıdan yukarıdaki bilgilerin girdi olarak alınması  
+Bileşik ve basit kâr formulü kullanılarak hesap yapılabilmesi  
+Geri ödeme planının bir tabloda gösterilmesi  
+React.js kütüphanesi kullanılarak statik bir web sitesi yapılabilmesi  
+
+```
+❗Not: Ekranların kullanıcı deneyimi odaklı olması, 
+görsellik ve hizalama konularına dikkat edilmesi 
+beklenmektedir.
+```
+
+# Beklenen Özellikler
+
+* Fonksiyonel komponentler kullanılması,
+* En az 2 adet React Context oluşturulması,
+* Herhangi bir seviyedeki component’in context’e erişim sağlayabilmesi,
+* Referans üzerinden componentin dışarıya açtığı özelliklere erişim sağlanabilmesi,
+* useRef, useImpretiveHandle, forwardRef kullanılması,
+* Kullanıcı gerekli bilgileri girdikten sonra, bir buton aracılığı ile toplam geri ödeme tutarı, aylık taksit tutarı ve toplam vergi tutarları gösterilebilir.
+* Girilen periyoda göre hesaplanan geri ödeme planı tablosu, bir buton aracılığı ile popup olarak gösterilebilir.
+
+# Formüller
+
+Kar oranı aylık alınmıştır.
+
+Basit → Kâr = ( Anapara * Kâr oranı * ( gün sayısı / 30 ))
+
+Bileşik → Kâr = ( Anapara * ( ( 1 + kâr oranı) ^ (gün sayısı / 30) ) ) - Anapara
+
+# Örnek Hesaplama
+
+* Kâr Oranı: %2.28 aylık
+
+* Anapara: 100,000.00 TL
+
+* Taksit Sayısı: 12
+
+* Taksit aralığı: Aylık
--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
+import { useState } from "react";
+
+
 const App = () => {
+    const [principal,setPrincipal] = useState("");
+    const [numberOfInstallments,setNumberOfInstallments] = useState("");
+    const [interestRate,setInterestRate] = useState("");
+    const [installmentFrequency,setInstallmentFrequency] = useState("");
+
+    const FREQUENCY = ["aylık","haftalık","yıllık"];
+
     return (
         <div>
         <h1>here we go again</h1>
+        <form className="user-inputs">
+            <label>Anapara(Kredi Tutarı)
+                <input
+                type="number"
+                placeholder="100.000,00 TL"
+                min="0"
+                step="1"
+                value={principal}
+                onChange={(e)=>setPrincipal(e.target.value)}
+                required
+                ></input>
+            </label>
+            <label>Taksit Sayısı
+                <input
+                type="number"
+                placeholder="0"
+                min="0"
+                step="1"
+                value={numberOfInstallments}
+                onChange={(e)=>setNumberOfInstallments(e.target.value)}
+                required
+                ></input>
+            </label>
+            <label>Kâr oranı
+                <input
+                type="number"
+                placeholder="%1,28"
+                min="0"
+                value={interestRate}
+                onChange={(e)=>setInterestRate(e.target.value)}
+                required
+                ></input>
+            </label>
+            <label>Taksit Aralığı
+                <select
+                value={installmentFrequency}
+                onChange={(e)=>setInstallmentFrequency(e.target.value)}
+                onBlur={(e)=>setInstallmentFrequency(e.target.value)}
+                >
+                    <option/>
+                    {FREQUENCY.map((option)=>(
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
+            </label>
+        </form>
         </div>
     )
 }
