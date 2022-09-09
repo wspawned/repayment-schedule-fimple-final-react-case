f4356bbb007c324f80c1e95ff6cb1040cf1d202e
wspawned <wspawned@gmail.com>
2022-09-09 22:33:31 +0300

    add context

--- src/App.js
+++ src/App.js
@@ -1,229 +1,34 @@
 import { useState } from "react";
-import { compoundInterestCalculation, simpleInterestCalculation } from "./financialUtil";
+import { createContext } from "react";
+import FormInput from "./Form";
+import { Table } from "./Table";
 
+const ResultContext = createContext();
 
-const App = () => {
-  const [principal, setPrincipal] = useState("");
-  const [interestRate, setInterestRate] = useState("");
-  const [paymentPeriod, setPaymentPeriod] = useState("");
-  const [interestType,setInterestType] = useState("");
-  const [numberOfInstallments, setNumberOfInstallments] = useState("");
-  const [kkdfTax, setKkdfTax] = useState("");
-  const [bsmvTax, setBsmvTax] = useState("");
+const ContextProvider = (props) => {
   const [tableInfo, setTableInfo] = useState([]);
 
-  const PAYMENT_FREQUENCY = ["Aylık", "Yıllık", "Haftalık"];
-  const INTEREST_TYPE = ["Bileşik Faiz","Basit Faiz"];
+  return (
+    <ResultContext.Provider value={{
+      tableInfo,
+      setTableInfo
+    }}>
+      {props.children}
+    </ResultContext.Provider >
+  )
+}
 
-  const compute = () => {
-    const monthlyInterestRate = Number(interestRate) / 100;
-    const numberOfPayment = Number(numberOfInstallments);
-    const principalValue = Number(principal);
-    const kkdf = Number(kkdfTax) / 100;
-    const bsmv = Number(bsmvTax) / 100;
-
-    let interestRateDuePeriod = 0;
-    if(paymentPeriod==="Haftalık") interestRateDuePeriod = monthlyInterestRate/4;
-    else if(paymentPeriod==="Yıllık") interestRateDuePeriod = monthlyInterestRate*12;
-    else interestRateDuePeriod = monthlyInterestRate;
-    
-    let paymentsList = [];
-    
-    if(interestType==="Basit Faiz") {
-      paymentsList = simpleInterestCalculation(principalValue, numberOfPayment, interestRateDuePeriod, kkdf, bsmv);
-    } else {
-      paymentsList = compoundInterestCalculation( principalValue, numberOfPayment, interestRateDuePeriod, kkdf, bsmv);
-    }
-    setTableInfo(paymentsList);
-  };
+const App = () => {
 
   return (
-    <div>
-      <div className="home-page">
-        <h1>Kredi Ödeme Planı <br/>Hesaplama</h1>
-        <form
-          className="user-inputs"
-          onSubmit={(e) => {
-            e.preventDefault();
-            compute();
-          }}
-        >
-          <label>
-            Kredi Tutarı (Anapara)
-            <input
-              type="number"
-              placeholder="100.000,00 TL"
-              min="0"
-              step="1"
-              value={principal}
-              onChange={(e) => setPrincipal(e.target.value)}
-              required
-            ></input>
-          </label>
-          <label>
-            Aylık Faiz Oranı (%)
-            <input
-              type="number"
-              placeholder="%2,28"
-              min="0"
-              step="0.01"
-              value={interestRate}
-              onChange={(e) => setInterestRate(e.target.value)}
-              required
-            ></input>
-          </label>
-          <label>
-            Taksit Aralığı
-            <select
-              value={paymentPeriod}
-              onChange={(e) => setPaymentPeriod(e.target.value)}
-              onBlur={(e) => setPaymentPeriod(e.target.value)}
-              required
-            >
-              <option />
-              {PAYMENT_FREQUENCY.map((option) => (
-                <option key={option} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label>
-            Faiz Türü
-            <select
-              value={interestType}
-              onChange={(e) => setInterestType(e.target.value)}
-              onBlur={(e) => setInterestType(e.target.value)}
-              required
-            >
-              <option />
-              {INTEREST_TYPE.map((option) => (
-                <option key={option} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label>
-            Taksit Sayısı
-            <input
-              type="number"
-              placeholder="12"
-              min="0"
-              step="1"
-              value={numberOfInstallments}
-              onChange={(e) => setNumberOfInstallments(e.target.value)}
-              required
-            ></input>
-          </label>
-          <div className="taxes">
-            <label>
-              KKDF (%)
-              <input
-                type="number"
-                placeholder="%15"
-                min="0"
-                step="0.01"
-                value={kkdfTax}
-                onChange={(e) => setKkdfTax(e.target.value)}
-                required
-              ></input>
-            </label>
-            <label>
-              BSMV (%)
-              <input
-                type="number"
-                placeholder="%10"
-                min="0"
-                step="0.01"
-                value={bsmvTax}
-                onChange={(e) => setBsmvTax(e.target.value)}
-                required
-              ></input>
-            </label>
-          </div>
-          <button type="submit">Hesapla</button>
-        </form>
-      </div>
-
-      {tableInfo.length ? (
-        <div className="table-container">
-          <div>
-            <table>
-              <tbody>
-                <tr>
-                  <th>Toplam Maliyet</th>
-                  <th>Toplam Faiz</th>
-                  <th>Toplam Vergi</th>
-                  <th>Taksit</th>
-                </tr>
-
-                <tr>
-                  <td>
-                    {tableInfo[tableInfo.length - 1].totalPayment.toFixed(2) +
-                      " TL"}
-                  </td>
-                  <td>
-                    {tableInfo[
-                      tableInfo.length - 1
-                    ].totalInterestPayment.toFixed(2) + " TL"}
-                  </td>
-                  <td>
-                    {tableInfo[tableInfo.length - 1].totalTaxPayment.toFixed(
-                      2
-                    ) + " TL"}
-                  </td>
-                  <td>
-                    {tableInfo[tableInfo.length - 1].installment.toFixed(2) +
-                      " TL"}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-
-          <h2>GERİ ÖDEME PLANI TABLOSU</h2>
-          <table>
-            <tbody>
-              <tr>
-                <th>Taksit No</th>
-                <th>Taksit Tutarı</th>
-                <th>Anapara</th>
-                <th>Kalan Anapara</th>
-                <th>Kar Tutarı</th>
-                <th>KKDF</th>
-                <th>BSMV</th>
-              </tr>
-
-              {tableInfo.map((item) => {
-                const key = item.paymentNo;
-                const paymentNo = item.paymentNo;
-                const installment = item.installment.toFixed(2);
-                const principalPayment = item.principalPayment.toFixed(2);
-                const remainingPrincipal = Math.abs(
-                  item.remainingPrincipal
-                ).toFixed(2);
-                const interestPayment = item.interestPayment.toFixed(2);
-                const kkdfPayment = item.kkdfPayment.toFixed(2);
-                const bsmvPayment = item.bsmvPayment.toFixed(2);
-
-                return (
-                  <tr key={key}>
-                    <td>{paymentNo}</td>
-                    <td>{installment}</td>
-                    <td>{principalPayment}</td>
-                    <td>{remainingPrincipal}</td>
-                    <td>{interestPayment}</td>
-                    <td>{kkdfPayment}</td>
-                    <td>{bsmvPayment}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      ) : null}
-    </div>
+    <ContextProvider>
+      <ResultContext.Consumer>
+        {(context) => <FormInput setTableInfo={context.setTableInfo} />}
+      </ResultContext.Consumer>
+      <ResultContext.Consumer>
+        {(context) => <Table tableInfo={context.tableInfo} />}
+      </ResultContext.Consumer>
+    </ContextProvider>
   );
 };
 
